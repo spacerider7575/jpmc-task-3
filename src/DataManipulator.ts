550b006b1103abfca96074b63e58b6b63ecd41fe
spacerider7575 <101892566+spacerider7575@users.noreply.github.com>
2023-01-29 23:39:22 +0530

    Update DataManipulator.ts

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,9 +1,13 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+      price_abc:number,
+      price_def:number,
+      ratio:number,
+      timestamp:Date,
+      upper_bound:number,
+      lower_bound:number,
+      trigger_alert:number|undefined,
 }
 
 
@@ -11,9 +15,13 @@ export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
     return serverResponds.map((el: any) => {
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        price_abc:priceABC,
+        price_def:priceDEF,
+        ratio,
+        timestamp:serverRespond[0].timestamp > servereRsepond[1].timestamp ?serverRespond[0].timestamp : servereRsepond[1].timestamp,
+        upper_bound:upperbound,
+        lower_bound:lowerbound,
+        trigger_alert:(ratio  > upperbound || ratio <lowerbound)? ratio : undefined,
       };
     })
   }
